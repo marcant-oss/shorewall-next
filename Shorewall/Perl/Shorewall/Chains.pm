c11b647b1bbcaf90e07d915c3f388d4cd5aac96e
Tom Eastep <teastep@shorewall.net>
2020-04-29 12:34:41 -0700

    Fix defect which prevented dynamic blacklist ipsets from being created
    Signed-off-by: Tom Eastep <teastep@shorewall.net>

--- Shorewall/Perl/Shorewall/Chains.pm
+++ Shorewall/Perl/Shorewall/Chains.pm
@@ -8893,7 +8893,7 @@ sub ensure_ipsets( @ ) {
     if ( $globals{DBL_TIMEOUT} ne '' && $_[0] eq $globals{DBL_IPSET} ) {
 	shift;
 
-	emit( qq(    if qt \$IPSET list $globals{DBL_IPSET}; then));
+	emit( qq(    if ! qt \$IPSET list $globals{DBL_IPSET}; then));
 
 	push_indent;
 
