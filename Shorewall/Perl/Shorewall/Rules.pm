84b2a76bfb8b97d36ca9ccd5571b5099f98ed598
Tom Eastep <teastep@shorewall.net>
2020-06-28 11:03:04 -0700

    Correct handling of ";;+" in the snat file
    Signed-off-by: Tom Eastep <teastep@shorewall.net>

--- Shorewall/Perl/Shorewall/Rules.pm
+++ Shorewall/Perl/Shorewall/Rules.pm
@@ -5572,6 +5572,14 @@ sub process_snat1( $$$$$$$$$$$$$ ) {
     my $logaction;
     my $param;
 
+    #
+    # Handle early matches
+    #
+    if ( $inlinematches =~ s/^s*\+// ) {
+	$prerule = $inlinematches;
+	$inlinematches = '';
+    }
+
     if ( $action =~ /^MASQUERADE(\+)?(?:\((.+)\))?$/ ) {
 	$target     = 'MASQUERADE';
 	$actiontype = $builtin_target{$action = $target};
